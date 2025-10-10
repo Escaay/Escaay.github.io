9c692cd53bebc10253b18fb59aff6d38c4c6560a
qiuwenjing <qiuwenjing03@meituan.com>
2025-10-10 10:33:14 +0800

    feat: xr-demo

--- assets/index-7f0d7e43.js
+++
@@ -1,1 +0,0 @@
-import{E as o}from"./index-d71e7d1d.js";function n(){return o.jsx("div",{children:"hobby"})}export{n as default};
--- assets/index-b3776d18.js
+++
@@ -1,1 +0,0 @@
-import{E as r}from"./index-d71e7d1d.js";function e(){return r.jsx("div",{children:"travel"})}export{e as default};
