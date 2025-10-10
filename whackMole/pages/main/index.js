a469d9f42c2dcefe1ba936a2357123b79bf70db7
qiuwenjing <qiuwenjing03@meituan.com>
2025-10-10 10:22:55 +0800

    feat: xr-demo

--- whackMole/pages/main/index.js
+++
@@ -1,5 +0,0 @@
-let start = (e) => {
-  let gameMode = e.target.className
-  window.location.href = `../game/index.html?gameMode=${gameMode}`
-  // pm.navigateTo({ url: 'pages/game/game', value: { gameMode: id } })
-}
